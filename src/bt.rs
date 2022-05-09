a6f421e7b3ae8bd1ff88f2ff0bde3f4355fd92fe
chrisculver <chris.cb.culver@gmail.com>
2022-05-09 12:07:28 +0100

    This actually seems to function in a somewhat reasonable way.  Instead of generic types we use Node types only.  This does require Mutex's...

--- src/bt.rs
+++ src/bt.rs
@@ -1,59 +1,34 @@
-use bevy::{
-    ecs::{component::Component, query::WorldQuery, world::Mut},
-    prelude::Query,
-};
+use bevy::ecs::component::Component;
+use bevy::prelude::Query;
+use std::sync::{Arc, Mutex};
 
-use std::fmt::Debug;
-use std::sync::Arc;
+// TODO: ATM this will lock up threads while waiting for the BT to be made available...
+// Could run ALL the AI on it's own thread?
+// How to avoid MuteX?
 
 #[derive(PartialEq, Eq)]
 pub enum Status {
-    NeverRun,
     Success,
     Failure,
     Running,
 }
 
-//  We want to build a behavior tree
-//
-//             Root
-//               v
-//            Sequence
-//        v -------- v ------- v
-//      Action1    Action2   Selector
-//                           v ----------v
-//                        Action3      Action4
 
-// and arbitrarily complicated...
-
-// The entity is the AI it's attached to.
-// The tree itself is a component
-// Ticking the tree is a system
-
-// So I want to do something like
-// commands.insert(BasicBot)
-//         .insert(BasicBotBehaviorTree);
-// app.add_system(tick_behavior_tree);
-
-#[derive(WorldQuery, Component)]
-#[world_query(mutable, derive(Debug))]
-pub struct BehaviorTree<'w, T: Tickable + Component> {
-    pub root: &'w mut T,
+pub trait Node {
+    fn tick(&mut self) -> Status;
 }
 
-// We should tick all trees!
-// We can add an option time/frame delay to not tick them every frame
-pub fn tick_behavior_trees<T: Tickable + Component>(mut bt_query: Query<&mut BehaviorTree<'static, T>>) {
-    for mut bt in bt_query.iter_mut() {
-        bt.root.tick();
-    }
+#[derive(Component)]
+pub struct BehaviorTree {
+    root: Arc<Mutex<dyn Node + Send + Sync>>,
 }
 
+#[derive(Component)]
 pub struct Action {
     pub func: Arc<dyn Fn() -> Status + Send + Sync>,
 }
 
-impl Tickable for Action {
+impl Node for Action {
     fn tick(&mut self) -> Status {
         return (self.func)();
     }
@@ -61,27 +36,71 @@ impl Tickable for Action {
 
 #[derive(Component)]
 pub struct Sequence {
-    // Of course sequences can have non Actions as children...
-    // Probalby needs a Vec of T where T is tickable?
-    pub children: Vec<Box<Action>>,
+    pub children: Vec<Arc<Mutex<dyn Node + Send + Sync>>>,
     pub active: usize,
 }
 
-impl Tickable for Sequence {
+// TODO: Test this is the correct behavior.
+impl Node for Sequence {
     fn tick(&mut self) -> Status {
-        let child_status = self.children[self.active].tick();
+        let child_status = self.children[self.active].lock().unwrap().tick();
         if child_status == Status::Success {
+            // if a child succeeds and they are not the last child go to next
             if self.active + 1 < self.children.len() {
                 self.active += 1;
+                return Status::Running;
             } else {
+                // all of the children succeeded so this node succeeds
                 return Status::Success;
             }
+        } else if child_status == Status::Failure {
+            self.active = 0;
         }
-
         return child_status;
     }
 }
 
-pub trait Tickable: Send + Sync {
-    fn tick(&mut self) -> Status;
+
+pub fn test_run_bts(mut bt_query: Query<&mut BehaviorTree>) {
+    for bt in bt_query.iter_mut() {
+        bt.root.lock().unwrap().tick();
+    }
+}
+
+
+pub fn create_basic_bt() -> BehaviorTree {
+    BehaviorTree {
+        root: Arc::new(Mutex::new(Sequence {
+            children: vec![
+                Arc::new(Mutex::new(Action {
+                    func: Arc::new(always_succeed),
+                })),
+                Arc::new(Mutex::new(Sequence {
+                    children: vec![
+                        Arc::new(Mutex::new(Action {
+                            func: Arc::new(always_succeed),
+                        })),
+                        Arc::new(Mutex::new(Action {
+                            func: Arc::new(always_succeed),
+                        })),
+                        Arc::new(Mutex::new(Action {
+                            func: Arc::new(always_fail),
+                        })),
+                    ],
+                    active: 0,
+                })),
+            ],
+            active: 0,
+        })),
+    }
+}
+
+pub fn always_succeed() -> Status {
+    println!("Action always succeed");
+    return Status::Success;
+}
+
+pub fn always_fail() -> Status {
+    println!("Action always fail");
+    return Status::Failure;
 }
